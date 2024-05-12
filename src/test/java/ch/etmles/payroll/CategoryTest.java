20729940ee5d51e19a43cc0d407d8e35bbbcb375
julie <juliecobas@outlook.com>
2024-05-12 17:00:42 +0200

    AjoutTest

---
+++ src/test/java/ch/etmles/payroll/CategoryTest.java
@@ -0,0 +1,9 @@
+package ch.etmles.payroll;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+public class CategoryTest {
+    @Test
+    void getname(){}
+}
